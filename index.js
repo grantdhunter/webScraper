4c304e40f98cfe911598eb6c5a9bd8cfcbe76fc4
Grant Hunter <info@grant-hunter.ca>
2014-10-03 21:34:19 -0600

    working IMDB scraping module, can accept custome scraper modules and has a backup generic scraper module

--- index.js
+++ index.js
@@ -2,74 +2,143 @@ var request = require('request');
 var cheerio = require('cheerio');
 var validUrl = require('valid-url');
 var URL = require('url');
+var Q = require('q');
 
-module.exports = {
-    crawlForData: function (url, className, callback) {
-        if (className.indexOf('.') < 0) {
-            className = '.' + className;
-        }
-
-        if (!validUrl.isUri(url)) {
-            console.log('Not a valid URL!');
-            return;
-        }
-        console.log('className: ' + className);
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var $ = cheerio.load(body);
-                var data = [];
-
-
-                $(className).each(function (i, elem) {
-                    //check if the class is nested and ignore top level
-                    if ($(elem).find(className).length <= 0) {
-                        var links = [];
-                        var imgs = []
-
-                        $($(this).find('a')).each(function (i, elem) {
-                            var link = $(elem).attr('href');
-
-                            if (!validUrl.isUri(link)) {
-                                if (link.indexOf('/') != 0) {
-                                    link = '/' + link;
-                                }
-                                link = URL.parse(url).protocol + '//' + URL.parse(url).hostname + link;
-                            }
-                            links.push(link);
-                        });
-
-                        $($(this).find('img')).each(function (i, elem) {
-                            var img = $(elem).attr('src');
-
-                            if (!validUrl.isUri(link)) {
-                                if (img.indexOf('/') != 0) {
-                                    img = '/' + img;
-                                }
-                                img = URL.parse(url).protocol + '//' + URL.parse(url).hostname + img;
-                            }
-                            imgs.push(img);
-                        });
-
-                        var content = $(elem).text().replace(/\s{2,9999}/g, ' ');
-
-
-                        //
-                        //                        request(link, function (error, response, body) {
-                        //                            if (!error && response.statusCode == 200) {
-                        //                            
-                        //                            }
-                        //                        });
-
-                        data.push({
-                            links: links,
-                            imgs: imgs,
-                            content: content
-                        });
-                    }
-                });
-                callback(data);
-            }
-        });
+function crawlForData(url, className, callback, crawlModule) {
+    if (className.indexOf('.') < 0) {
+        className = '.' + className;
     }
 
-};
+    if (!validUrl.isUri(url)) {
+        console.log('Not a valid URL!');
+        return;
+    }
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(body);
+            var classContent = [];
+            var promises = [];
+            var deferred = Q.defer();
+
+            $(className).each(function (i, elem) {
+                //check if the class is nested and ignore top level
+                if ($(elem).find(className).length <= 0) {
+
+                    var promise;
+
+                    //for custom content you want parsed from the URL
+                    if (crawlModule) {
+                        promise = crawlModule(elem);
+                        promise.then(function (data) {
+                            classContent.push(data);
+                        });
+                        promises.push(promise);
+                    } else {
+                        //some premade crawlModules
+                        switch (URL.parse(url).hostname) {
+                        case 'www.imdb.com':
+                            //async process
+                            promise = imdbCrawlModule(elem);
+                            promise.then(function (data) {
+                                classContent.push(data);
+                            });
+                            promises.push(promise);
+                            break;
+                        default:
+                            //generic content scraping
+                            //this is syncronous
+                            promise = genericCrawlModule(elem);
+                            promise.then(function (data) {
+                                classContent.push(data);
+                            });
+                            promises.push(promise);
+                        }
+                    }
+                }
+            });
+
+            Q.all(promises).spread(function () {
+                callback(classContent)
+            }).done();
+        }
+    });
+
+    //get all URLS inside the 
+    function getAllLinks(element, tag, attribute) {
+        var links = [];
+        var $ = cheerio.load(element);
+        $($(element).find(tag)).each(function (i, elem) {
+            var link = $(elem).attr(attribute);
+
+            link = normalizeLink(link);
+
+            links.push(link);
+        });
+        return links
+    }
+
+    function normalizeLink(link) {
+        if (link) {
+            if (!validUrl.isUri(link)) {
+                if (link.indexOf('/') != 0) {
+                    link = '/' + link;
+                }
+                link = URL.parse(url).protocol + '//' + URL.parse(url).hostname + link;
+            }
+        }
+        return link;
+    }
+
+    function genericCrawlModule(element) {
+        var $ = cheerio.load(element);
+        var deferred = Q.defer();
+
+        var tempContent = {
+            links: getAllLinks(element, 'a', 'href'),
+            imgs: getAllLinks(element, 'img', 'src'),
+            content: $(element).text().replace(/\s{2,9999}/g, ' ').trim()
+        };
+
+        deferred.resolve(tempContent);
+
+        return deferred.promise;
+    }
+
+
+    function imdbCrawlModule(element) {
+        var links = getAllLinks(element, 'a', 'href');
+        var deferred = Q.defer();
+
+        request(links[0], function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                var $ = cheerio.load(body);
+
+                var title = $(body).find('[itemprop="name"]').first().text().trim();
+                var desc = $(body).find('[itemprop="description"]').text().replace(/\s{2,9999}/g, ' ').trim();
+                var image = $(body).find('[itemprop="image"]').attr('src');
+                image = normalizeLink(image);
+                var trailer = $(body).find('[itemprop="trailer"]').attr('href');
+                trailer = normalizeLink(trailer);
+                var link = links[0];
+
+                deferred.resolve({
+                    title: title,
+                    desc: desc,
+                    image: image,
+                    trailer: trailer,
+                    url: link
+                });
+            } else {
+                deferred.reject(new Error(error));
+            }
+        });
+        return deferred.promise;
+    }
+}
+
+
+module.exports = {
+    crawlForData: crawlForData
+}
